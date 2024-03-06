963368ac6d083548f94e6fe1fb487130ec34b1c3
quizford <125616425+Astronaut828@users.noreply.github.com>
2024-03-06 16:26:00 +0000

    Clean up script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,17 +7,17 @@
 const hre = require("hardhat");
 
 async function main() {
-  const ep = await hre.ethers.deployContract("EntryPoint");
-
-  await ep.waitForDeployment();
-
-  console.log(`Ep deployed to ${ep.target}`);
-
   const af = await hre.ethers.deployContract("AccountFactory");
 
   await af.waitForDeployment();
 
   console.log(`AF deployed to ${af.target}`);
+
+  const ep = await hre.ethers.deployContract("EntryPoint");
+
+  await ep.waitForDeployment();
+
+  console.log(`Ep deployed to ${ep.target}`);
 
   const pm = await hre.ethers.deployContract("Paymaster");
 
--- scripts/execute.js
+++ scripts/execute.js
@@ -4,7 +4,6 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const { EntryPoint__factory } = require("@account-abstraction/contracts");
 const hre = require("hardhat");
 
 const FACTORY_NONCE = 1;
